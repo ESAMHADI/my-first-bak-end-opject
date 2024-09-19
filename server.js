3644ca92c91457097dd89b06c9bd5d6709659212
ESAMHADI <alshawshsam48@gmail.com>
2024-09-19 09:41:53 +0300

    mm

--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 // server.listen(4000)
 
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,  // Use the DATABASE_URL from the .env file
+  ssl: { rejectUnauthorized: false }          // Add this for Render (SSL)
+});
 
 const express = require('express');
 const bodyParser = require('body-parser');
